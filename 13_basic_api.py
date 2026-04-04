426cbd4da2cc9984a5b0454d0a4a10e6598d41eb
Daniil <easyitwithdaniil@gmail.com>
2026-04-04 15:28:05 +0300

    add 13

--- 13_basic_api.py
+++ 13_basic_api.py
@@ -18,6 +18,8 @@ curl 'https://api.bank.easyitlab.tech/students/employees' \
   -H 'sec-fetch-site: same-site' \
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36'
 """
+
+
 """
 GET
 POST
@@ -51,44 +53,94 @@ DELETE
 """
 
 import requests
+from faker import Faker
+from requests import Response
+
+fake = Faker()
+
+
 
 BASE_URL = 'https://api.bank.easyitlab.tech'
-TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxYjFkZjBjYy1mMjIzLTRlZDMtODA4MS0yMjE1NTM2ZjhkZmMiLCJ1c2VyX2lkIjoiMWIxZGYwY2MtZjIyMy00ZWQzLTgwODEtMjIxNTUzNmY4ZGZjIiwic3lzdGVtX3JvbGUiOiJTVFVERU5UIiwiYnVzaW5lc3Nfcm9sZSI6IkNMSUVOVCIsInBlcm1pc3Npb25zIjpbImNsaWVudDphY2NvdW50czpyZWFkIiwiY2xpZW50OnRyYW5zZmVyczpjcmVhdGUiLCJjbGllbnQ6dGlja2V0czpjcmVhdGUiXSwic2Vzc2lvbl9pZCI6IjQyMGVkZjViNjg2ZTQwNzZiNDZlYzlkMzBkOTExZWU2IiwiaWF0IjoxNzc1MTUwOTIyLCJleHAiOjE3NzUxNTE4MjIsInR5cGUiOiJhY2Nlc3MifQ.StOyWjwyGOQOnWSqbMwVL3rw7Gdnwm9yQjkfu1cpWFM'
 
-# headers = {
-#     'Authorization': f'Bearer {TOKEN}'
-# }
-#
-# response = requests.get(
-#     url=f"{BASE_URL}/students/employees",
-#     headers=headers
-# )
-#
-# print(response.status_code)
-# print(type(response.json()))
-# print(response.json())
+EMAIL = "gravitel0nd@gmail.com"
+PASSWORD = "XH0RvnaAhSZt"
 
-headers = {
+login_response = requests.post(
+    f"{BASE_URL}/auth/login",
+    json={"email": EMAIL, "password": PASSWORD},
+    timeout=15,
+)
+login_response.raise_for_status()
+tokens = login_response.json()
+TOKEN = tokens["access_token"]
+
+
+HEADERS = {
     'Authorization': f'Bearer {TOKEN}',
     'Content-Type': 'application/json'
 }
 
-body = {
-    "email": "dasvsdvdqwdsfsdd@asfas.ru",
-    "full_name": "Daniil Nikolaev"
-}
+def create_employee():
+    body = {
+        "email": fake.ascii_company_email(),
+        "full_name": "Daniil Nikolaev"
+    }
+    created_employee = requests.post(
+        url=f"{BASE_URL}/students/employees",
+        headers=HEADERS,
+        json=body
+    )
+    assert created_employee.status_code == 200
+    employee_id = created_employee.json().get('id')
+    return employee_id
 
-created_employee = requests.post(
-    url=f"{BASE_URL}/students/employees",
-    headers=headers,
-    json=body
-)
-created_employee_json = created_employee.json()
+def delete_employee(employee_id):
+    requests.delete(
+        f"{BASE_URL}/students/employees/{employee_id}",
+        headers={
+            'Authorization': f'Bearer {TOKEN}'
+        }
+    )
 
-print(created_employee.status_code)
-print(type(created_employee_json))
-print(created_employee_json)
+def get_id(get_employee: Response):
+    get_employee_json = get_employee.json()
+    employee_id = get_employee_json.get('id')
+    assert employee_id is not None
+    return employee_id
 
-assert created_employee.status_code == 200, "Не успешно"
-assert isinstance(created_employee_json, dict)
-assert created_employee_json.get('id') is not None
+
+def test_employee():
+    employee_id = create_employee()
+
+    get_employee = requests.get(
+        f"{BASE_URL}/students/employees/{employee_id}",
+        headers=HEADERS
+
+    )
+    assert get_employee.status_code == 200
+    assert get_id(get_employee) == employee_id
+
+    name = fake.name()
+
+    body = {
+        "email": fake.ascii_company_email(),
+        "full_name": name
+    }
+
+    patch_employee = requests.patch(
+        f"{BASE_URL}/students/employees/{employee_id}",
+        json= body,
+        headers=HEADERS
+    )
+    assert get_employee.status_code == 200
+
+    get_employee = requests.get(
+        f"{BASE_URL}/students/employees/{employee_id}",
+        headers=HEADERS
+    )
+    assert get_employee.status_code == 200
+    get_employee_json = get_employee.json()
+
+    assert get_employee_json['full_name'] == name
+
+    delete_employee(employee_id)
