4f1e4d4f7fe060498682de8e8911398e2281560a
Daniil Nikolaev <danilfg@yahoo.com>
2026-07-18 14:24:55 +0300

    add credentials

---
+++ lesson_24_SQL_python/credentials.example.py
@@ -0,0 +1,16 @@
+BASE_URL = 'https://api.bank.easyitlab.tech'
+
+STUDENT_LOGIN = ""
+STUDENT_PASSWORD = ""
+
+SSH_HOST = "student.bank.easyitlab.tech"
+SSH_PORT = 22
+SSH_USER = ""
+SSH_PASSWORD = ""
+
+REMOTE_POSTGRES_HOST = "127.0.0.1"
+REMOTE_POSTGRES_PORT = 15432
+
+POSTGRES_DB = "demobank_student"
+POSTGRES_USER = ""
+POSTGRES_PASSWORD = ""
